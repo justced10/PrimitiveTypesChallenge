37fd702880c12aeb603b4c92eec1acc84830c622
Giovanni <maxiced001@warren.k12.in.us>
2024-09-24 12:55:31 -0400

    Initial commit

---
+++ src/VariableChallenge.java
@@ -0,0 +1,41 @@
+public class VariableChallenge {
+    public static void main(String[] args) {
+
+        // Byte variable
+
+        byte myByte = 10;
+
+
+
+        // Short variable
+
+        short myShort = 20;
+
+
+
+        // Int variable
+
+        int myInt = 30;
+
+
+
+        // Long variable
+
+        long myLong = 80000 - 5 * (myByte + myShort + myInt);
+
+
+
+        // Output the results
+
+        System.out.println("Byte value: " + myByte);
+
+        System.out.println("Short value: " + myShort);
+
+        System.out.println("Int value: " + myInt);
+
+        System.out.println("Long value: " + myLong);
+
+    }
+
+}
+
